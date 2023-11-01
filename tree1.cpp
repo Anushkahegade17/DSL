dd903a8759051426ddba4865fa8e31fb05ac62aa
anushkahegade <146116087+Anushkahegade17@users.noreply.github.com>
2023-11-01 09:34:26 +0530

    Add files via upload

---
+++ tree1.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create a new tree node
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a new node into the BST
+struct TreeNode* insert(struct TreeNode* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+// Function to perform an in-order traversal of the tree
+void inOrderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+
+    // Insert some data into the tree
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    // Perform in-order traversal to display the elements in sorted order
+    printf("In-order traversal of the tree: ");
+    inOrderTraversal(root);
+
+    // Clean up memory by freeing allocated nodes (not shown in this simple example)
+    
+    return 0;
+}
+
---
+++ tree2.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a binary tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create a new tree node
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a new node into the BST
+struct TreeNode* insert(struct TreeNode* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+// Function to perform an in-order traversal of the tree
+void inOrderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+// Function to perform a pre-order traversal of the tree
+void preOrderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrderTraversal(root->left);
+        preOrderTraversal(root->right);
+    }
+}
+
+// Function to perform a post-order traversal of the tree
+void postOrderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        postOrderTraversal(root->left);
+        postOrderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+// Function to search for a value in the BST
+struct TreeNode* search(struct TreeNode* root, int key) {
+    if (root == NULL || root->data == key) {
+        return root;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    }
+
+    return search(root->right, key);
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+
+    // Insert some data into the tree
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 70);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    // Perform in-order traversal to display the elements in sorted order
+    printf("In-order traversal of the tree: ");
+    inOrderTraversal(root);
+    printf("\n");
+
+    // Perform pre-order traversal
+    printf("Pre-order traversal of the tree: ");
+    preOrderTraversal(root);
+    printf("\n");
+
+    // Perform post-order traversal
+    printf("Post-order traversal of the tree: ");
+    postOrderTraversal(root);
+   
+
+    // Search for a value in the tree
+   
+
